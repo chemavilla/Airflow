59a69f572f1a893143f75ffa6cf639564c49a5e7
Jose Maria Villafañe Iravedra <85357729+chemavilla@users.noreply.github.com>
2024-01-23 20:02:40 +0100

    making progress

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,19 +1,21 @@
+'''
+Test DAG for Airflow 
+
+This DAG serves as a test example for Airflow webserver and 
+scheduler to verify that the installation is successful.
+'''
+
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
-# Define the DAG
-dag = DAG(
-    'test_dag',
-    description='A simple test DAG',
-    start_date=datetime(2022, 1, 1),
-    schedule_interval='@daily',
-)
-
-# Define the tasks
-task1 = DummyOperator(task_id='task1', dag=dag)
-task2 = DummyOperator(task_id='task2', dag=dag)
-task3 = DummyOperator(task_id='task3', dag=dag)
+# Define the DAG, 0 for sorting purposes 
+with DAG(dag_id='0_test_dag',
+         start_date=datetime(2024, 1, 21),
+         schedule='@daily') as dag:
+    task1=EmptyOperator(task_id='task1')
+    task2=EmptyOperator(task_id='task2')
+    task3=EmptyOperator(task_id='task3')
 
 # Define the task dependencies
 task1 >> task2 >> task3
---
+++ dags/test_operators_dag.py
@@ -0,0 +1,35 @@
+'''
+Test Operators DAG for Airflow 
+
+This DAG serves as a test example for Airflow Operators.
+'''
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator  
+
+from datetime import datetime
+
+# Define the DAG arguments
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 21),
+}
+
+def print_exec_date(execution_date, **context):
+    print('Execution date = ', execution_date)
+
+
+
+with DAG(dag_id='0_operators_dag',
+         default_args=default_args,
+         schedule='@daily') as dag:
+    
+    task1 = BashOperator(task_id='task1', bash_command='echo "Hello, world"')
+    task2 = BashOperator(task_id='task2', bash_command='echo "Hello, Airflow"')
+    
+    print_exec_date = PythonOperator(task_id="print_exec_date", python_callable=print_exec_date
+    )
+
+# Define the dependencies between the tasks
+task1 >> task2 >> print_exec_date
