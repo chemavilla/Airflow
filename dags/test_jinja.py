14befff2150462cd3feef58a5c0133a211f832e5
Jose Maria Villafañe Iravedra <85357729+chemavilla@users.noreply.github.com>
2024-02-11 19:51:34 +0100

    update test_jinja_macro

--- dags/test_jinja.py
+++ dags/test_jinja.py
@@ -2,6 +2,7 @@ from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 from default_args import common_args
 
+#TODO: Work in progress, tutorial https://marclamberti.com/blog/templates-macros-apache-airflow/ 
 @dag(schedule='@daily',
     start_date=datetime(2024, 2, 1),
     tags=["test"],
@@ -9,6 +10,7 @@ from default_args import common_args
 def jinja_macro():
     @task()
     def task1():
-        return 'Hello'
+        pass
+
 
 jinja_macro()
